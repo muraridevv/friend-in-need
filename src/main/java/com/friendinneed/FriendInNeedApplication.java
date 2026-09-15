cf07da96b525ca783aa348a8175eda5f389edb62
Murari Kumar <muraridevv@gmail.com>
2026-09-15 18:55:33 +0530

    Add simulated robot hardware controls

--- src/main/java/com/friendinneed/FriendInNeedApplication.java
+++ src/main/java/com/friendinneed/FriendInNeedApplication.java
@@ -6,9 +6,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import com.friendinneed.embedding.EmbeddingProperties;
 import com.friendinneed.voice.VoiceProperties;
+import com.friendinneed.robot.RobotProperties;
 
 @EnableScheduling
-@EnableConfigurationProperties({EmbeddingProperties.class, VoiceProperties.class})
+@EnableConfigurationProperties({EmbeddingProperties.class, VoiceProperties.class, RobotProperties.class})
 @SpringBootApplication
 public class FriendInNeedApplication {
   public static void main(String[] args) { SpringApplication.run(FriendInNeedApplication.class, args); }
